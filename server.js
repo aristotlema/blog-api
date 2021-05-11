35a8fb67dcde19c13ce8f23a49e10fcbea62218e
aristotlema <aristotle.maule@hotmail.com>
2021-05-10 22:07:55 -0400

    more updates to basic structure with sequelize

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
 require('dotenv').config();
-
 const express = require('express');
-const Sequelize = require('sequelize');
+const { sequelize, Post } = require('./models');
 const app = express();
 
 
 app.use(express.json());
 
 
-const sequelize = new Sequelize('blog_database', 'postgres', 'password', {
-    host: 'localhost',
-    dialect: 'postgres',
+// Get - all posts
+app.get('/posts', async (req, res) => {
+    try {
+        const posts = await Post.findAll();
+        res.json(posts);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+// Post - Posts
+app.post('/posts', async (req, res) => {
+    console.log('/POST - posts');
+    const { title, body } = req.body;
+    console.log(req.params);
+    try {
+        const post = await Post.create({ title: title, body: body });
+        res.json(post);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 
-const testDBConnection = async () => {
+app.listen({ port: 5000 }, async () => {
+    console.log('Server running on port 5000');
     try {
         await sequelize.authenticate();
-        console.log('Connection has been established successfully.');  
-    } catch (err) {
-        console.error('Unable to connect to the database:', error);
+        console.log('db connected');
+    } catch(err) {
+        console.error('Connection to DB failed', err);
     }
-}
-testDBConnection();
-
-
-
-app.listen(4001, () => console.log('Server Started'));
+});
